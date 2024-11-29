5c4f566d93d438f5f3fdc7eb9ee601527bba6bfd
g2su <g2su@ucsd.edu>
2024-11-29 10:52:53 -0800

    fix json

--- tests/test_json.py
+++ tests/test_json.py
@@ -1,4 +1,5 @@
 import ipytest
+import re
 import json
 from hypothesis import given, settings
 import sys
@@ -10,6 +11,23 @@ sys.path.insert(
 )
 
 from hypothesis_cfg import cfg  # type: ignore
+
+
+def fix_dup_keys_string(json_str: str):
+    key_pattern = r'(?<!\\)"(.*?)"(?:\s*:)'
+    seen_keys = {}
+
+    def replace_key(match):
+        key = match.group(1)
+        if key in seen_keys:
+            seen_keys[key] += 1
+            return f'"{key}_{seen_keys[key]}":'
+        else:
+            seen_keys[key] = 1
+            return f'"{key}":'
+
+    modified_json = re.sub(key_pattern, replace_key, json_str)
+    return modified_json
 
 
 def contains_type(json_obj, search_type):
@@ -24,12 +42,14 @@ def contains_type(json_obj, search_type):
 
 @given(cfg("tests/cfgs/json.cfg", max_depth=20))
 def test_json_inverse(json_str: str):
+    json_str = fix_dup_keys_string(json_str)
     obj = json.loads(json_str)
     assert json.dumps(obj).replace(" ", "") == json_str
 
 
 @given(cfg("tests/cfgs/json.cfg", max_depth=20))
 def test_json_keys_exist(json_str: str):
+    json_str = fix_dup_keys_string(json_str)
     obj = json.loads(json_str)
     for key in obj.keys():
         assert ('"' + key + '"') in json_str
@@ -37,6 +57,7 @@ def test_json_keys_exist(json_str: str):
 
 @given(cfg("tests/cfgs/json.cfg", max_depth=20))
 def test_json_dict_list_consistency(json_str: str):
+    json_str = fix_dup_keys_string(json_str)
     assert ("[" in json_str) == contains_type(json.loads(json_str), list)
     assert (json_str.count("{") > 1) == contains_type(json.loads(json_str), dict)
 
