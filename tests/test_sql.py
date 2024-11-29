d6b3a0f2b5b2bc79436a31cd685e83433f2a6462
maniknarang <narang_manik@rocketmail.com>
2024-11-28 16:57:50 -0800

    fix sql tests by sorting queried results

--- tests/test_sql.py
+++ tests/test_sql.py
@@ -16,30 +16,27 @@ sys.path.insert(
 from sql import setup_dbs, execute_query_sqlite3, execute_query_duckdb  # type: ignore
 
 @given(cfg("tests/cfgs/sql1.cfg", max_depth=20))
-@settings(deadline=1000)
 def test_sql_query_syntax(input: str):
     """Property 1: test generated SQL query syntax in SQLite3 DB"""
     result = execute_query_sqlite3(input)
     assert 'Error' not in str(result), f'Query failed to execute with error {str(result)}'
 
 @given(cfg("tests/cfgs/sql2.cfg", max_depth=20))
-@settings(deadline=1000)
 def test_sql_query_where_clause(input: str):
     """Property 2: test WHERE clause and verify SQLite3 and DuckDB return the same results"""
     result_sqlite3 = execute_query_sqlite3(input)
     result_duckdb = execute_query_duckdb(input)
     print('SQLite3 output:', result_sqlite3)
     print('DuckDB output:', result_duckdb)
-    assert result_sqlite3 == result_duckdb
+    assert sorted(result_sqlite3) == sorted(result_duckdb)
 
 @given(cfg("tests/cfgs/sql3.cfg", max_depth=20))
-@settings(deadline=5000)
 def test_sql_query_group_by_clause(input:str):
     """Property 3: test GROUP BY clause and verify SQLite3 and DuckDB return the same results"""
     result_sqlite3 = execute_query_sqlite3(input)
     result_duckdb = execute_query_duckdb(input)
     print('SQLite3 output:', result_sqlite3)
     print('DuckDB output:', result_duckdb)
-    assert result_sqlite3 == result_duckdb
+    assert sorted(result_sqlite3) == sorted(result_duckdb)
 
 ipytest.run("-s")
